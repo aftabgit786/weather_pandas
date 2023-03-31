2507ea6f7dc6f0a08b34f854080afbb4660d8222
aftabgit786 <gb485585@gmail.com>
2023-03-31 17:34:41 +0500

    update

--- t3_pandas.py
+++ t3_pandas.py
@@ -3,6 +3,5 @@ import pandas as pd
 data = pd.read_csv('f2.csv')
 
 events = data[data[' Events'].isin(['Snow', 'Rain', 'Rain-Snow'])]['PKT']
-get_dates = events.tolist()
 
-print(get_dates)
+print(events)
