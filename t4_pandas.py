54018e736dceef2c4856e30d697703283656d6a1
aftabgit786 <gb485585@gmail.com>
2023-04-01 10:44:39 +0500

    update

--- t4_pandas.py
+++ t4_pandas.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 data = pd.read_csv('f2.csv')
 
-get_thunderstorm = data[data[' Events'] == 'Thunderstorm']
-date = pd.to_datetime(get_thunderstorm['PKT'])
+data_thunderstorm = data[data[' Events'] == 'Thunderstorm']
+date = pd.to_datetime(data_thunderstorm['PKT'])
 day_names = date.dt.strftime('%A')
 
 print(day_names)
